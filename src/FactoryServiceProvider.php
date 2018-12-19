1d2c57cedc4f7f4fd497881b04488d99a4a19a23
Rasmus Nielsen <rasmus@easify.dk>
2018-12-19 13:28:21 +0100

    Update composer name + auto swap native laravel

--- src/FactoryServiceProvider.php
+++ src/FactoryServiceProvider.php
@@ -3,16 +3,16 @@
 namespace Makeable\LaravelFactory;
 
 use Faker\Generator;
+use Illuminate\Database\Eloquent\Factory as EloquentFactory;
 use Illuminate\Support\ServiceProvider;
 
 class FactoryServiceProvider extends ServiceProvider
 {
     public function register()
     {
+        $this->app->bind(EloquentFactory::class, Factory::class);
         $this->app->singleton(Factory::class, function ($app) {
-            return Factory::construct(
-                $app->make(Generator::class), $this->app->databasePath('factories')
-            );
+            return Factory::construct($app->make(Generator::class), $this->app->databasePath('factories'));
         });
     }
 

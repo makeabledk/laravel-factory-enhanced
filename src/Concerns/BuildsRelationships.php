e1d9fc58b21729939992a94cba779fb1091b3636
Rasmus Nielsen <rasmus@easify.dk>
2021-02-18 16:08:11 +0100

    Apply fixes from StyleCI (#39)

--- src/Concerns/BuildsRelationships.php
+++ src/Concerns/BuildsRelationships.php
@@ -6,7 +6,6 @@ use Closure;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
-use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Database\Eloquent\Relations\HasOneOrMany;
 use Illuminate\Database\Eloquent\Relations\MorphOne;
 use Illuminate\Database\Eloquent\Relations\MorphOneOrMany;
